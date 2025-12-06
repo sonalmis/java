a10a6698bb35c4ba89421ecf51a5713100acf461
sonalmis <48862310+sonalmis@users.noreply.github.com>
2025-12-06 15:00:59 +0530

    Fix typo in HelloWorld.java comment
    Corrected a typo in the comment.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, I am here in the terminal window in git bash local.CI chsnges
+      // Prints "Hello, I am here in the terminal window in git bash local.CI chsnges by me
       System.out.println("Hello all, Shahataaz and Yogesh");
    }
 }
